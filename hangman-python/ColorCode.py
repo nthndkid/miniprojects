437810e0ef81237eff3567423db389fde730a16b
nthndkid <143003322+nthndkid@users.noreply.github.com>
2023-09-06 22:47:18 +0800

    Add files via upload

---
+++ hangman-python/ColorCode.py
@@ -0,0 +1,10 @@
+
+Reset = "\033[0m"
+Bold       = "\033[1m"
+LightRed     = "\033[91m"
+Underlined = "\033[4m"
+Red          = "\033[31m"
+Green        = "\033[32m"
+Yellow       = "\033[33m"
+LightGray    = "\033[37m"
+BackgroundWhite        = "\033[107m"
